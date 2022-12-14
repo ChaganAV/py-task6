bbea007b48af04ebb723739144fd39dc2ee6738c
Alexandr <jwchaganav@gmail.com>
2022-12-14 19:08:28 +0400

    настроил любое количество знаков

--- py-task6.py
+++ py-task6.py
@@ -3,12 +3,10 @@ sum1 = 0
 sum2 = 0
 count = 0
 number = int(sNumber)
-if number//2 != 0:
+if len(sNumber)%2 != 0:
     print("Сожалею, вам выпал не счастливый билетик :(")
 else:
     middle = len(sNumber)//2
-    
-    min = 99999
     max = 10**len(sNumber)
     while number > 0:
         max = max//10
@@ -22,3 +20,5 @@ else:
         print("Поздравляю, вам выпал счастливый билетик, возмите с полки пирожок :)")
     else:
         print("Сожалею, вам выпал не счастливый билетик :(")
+
+
